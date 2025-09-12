42e825177a757391d49469aa5f54b1689a04d10a
makag02 <makalapuag002@gmail.com>
2025-09-12 12:47:14 -0700

    Added intro etc.

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -12,15 +12,17 @@ export const AboutSection = () => {
         <div className="container mx-auto max-w-5x">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Computer Science Student @ University of Washington
+              4th year Computer Science Student @ University of Washington
             </h3>
 
             <p className="text-muted-foreground">
-              Intro
+              I spent 7 years in the United States Air Force as an aircraft mechanic.
+              I've always been passionate for anything to do with tech and purused my studies in Computer Science in 2022.
+              I transferred and got accepted into The Allen School of Computer Science & Engineering in 2024. I plan on graduating in Summer 2026 but am 100% willing to take an internship before hand.
             </p>
 
             <p className="text-muted-foreground">
-              What I'm seeking
+              I am currently seeking internships and/or junior dev roles.
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
@@ -30,7 +32,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/resume.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
